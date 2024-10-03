e3eb1e8e53c5bb4132b6a10c37cc3719b124a293
Fernanda Louro <fernandaalves.tds2.senai@gmail.com>
2024-10-03 09:01:36 -0300

    refactor: alter server to use routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const routes = Router();
 
 routes.get("/", (req, res) => {
-    return res.status(200).send({ message: "Haleluia!"})
+    return res.status(200).send({ message: "Eu ouvo música!"})
 });
 
 export default routes;
